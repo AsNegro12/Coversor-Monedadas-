57730d12174e08196a3e546d8b7f4da32fe1c76b
Uriel <floresruizur13l@outlook.com>
2023-07-19 19:54:29 -0700

    Se acreado la ventana

--- src/mx/Uriel/ConvesorDeDivisas/EntradaDeValor.java
+++ src/mx/Uriel/ConvesorDeDivisas/EntradaDeValor.java
@@ -10,6 +10,11 @@ public class EntradaDeValor
 		//El tipo de divisa que uso como base para convertir es el peso mexica
 		JOptionPane.showInputDialog(null,"Ingresa la cantidad de dinero que deseas convetir: ",
 				"Entrada de valor",JOptionPane.QUESTION_MESSAGE);
+		
+		Object[] monedas = {"peso(MXN)","Dolar(USD)","Euro",
+				"Libra esterlina","Yen Japonés","Won surcoreano"};
+		
+		JOptionPane.showMessageDialog(null, monedas);
 	}
 
 }
