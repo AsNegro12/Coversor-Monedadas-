52b94022a4157ff1a4d073ba391aee531d6ee60f
Uriel <floresruizur13l@outlook.com>
2023-07-24 20:02:12 -0700

    Se termino de comentar el codigo

--- src/mx/Uriel/MenuPrincipal/GeneradorDeListas.java
+++ src/mx/Uriel/MenuPrincipal/GeneradorDeListas.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 public class GeneradorDeListas 
 {
-	
+	//Para ahorrar codigo, aqui se crean los campos de texto 
+	//para los JOpitionPane.showInputDialog() que se usan para mostrar
+	//los menus de temperatura y monedas.
 	List<String> opciones = new ArrayList<>();
 	
+	//Esta funcion es llamada por un Array de tipo Object para rellenar
+	//los menus de MostrarVentanaTemp() y VentanaMonedas().
 	public List<String> getOpciones() {
 		return opciones;
 	}
 	
-	public List<String> setOpciones(List<String> opciones) {
-		return opciones;
-	}
-	public void listas(String moneda)
+	//Crea las listas para MostrarVentanaTemp() y VentanaMonedas().
+	public void listas(String texto)
 	{
-		opciones.add(moneda);
+		opciones.add(texto);
 		//System.out.println(opciones+" "+valorTem);
 	}
 	
--- src/mx/Uriel/MenuPrincipal/OperacionesDeComversiones.java
+++ src/mx/Uriel/MenuPrincipal/OperacionesDeComversiones.java
@@ -12,15 +12,17 @@ public class OperacionesDeComversiones
 
 	public void errorMenorA1(double valor) throws ValorMenorA1Exception
 	{
-		if(valor <= this.valor)
+		if(valor <= this.valor)//Aqui comprueba si valor es menor igual a 0.
 		{
+			//Si lo se lanza el siguiente mesaje.
 			throw new ValorMenorA1Exception("Solo se permiten valores mayores a 1.");
 		}
 		
 	}
-	
-	//dolar(US): 16.65, euro: 18.80, libra: 21.74, yen: 0.12, won: 0.013 valores a 20/jul/2023
-	
+	//
+	//Las siguientes funciones hace conversiones de de monedas
+	//
+	//Aqui se hacer conversion de pesos(MXN) a otras monedas.
 	public void ConversionPesosAOtrasMonedas(int valor)
 	{
 		//dolar(US): 16.65, euro: 18.80, libra: 21.74, 
@@ -29,11 +31,14 @@ public class OperacionesDeComversiones
 		resultado = getValor()/monedas[valor];	
 	}
 	
+	//Aqui se hacer conversion de otras monedas a pesos(MXN).
 	public void ConversorOtrasMonedasAPesos(int valor)
 	{
 			resultado = getValor()*monedas[valor];	
 	}
-	
+	//
+	//Las siguientes funciones hace conversiones de temperatura
+	//
 	public void CelsiusAFarenheit(double valor)
 	{
 		resultado = ((getValor()*9)/5)+32;
@@ -64,9 +69,14 @@ public class OperacionesDeComversiones
 		resultado = (((getValor()-273.15)*9)/5)+32;
 	}
 	
+	//Esta funcion imprime los resultados de las conversiones
 	public void ImprimirResuldado(String string, boolean valor)
 	{
-		
+		/*
+		 *  Este if se usa para saber si la conversion es de monedas o temperaturas.
+		 *  Si es false asigna el valor "Tienes: $" para conversiones de monedas.
+		 *  y sino "Son: " para converosres de temperatura.
+		 */
 		if(valor == false)
 		{
 			es = "Tienes: $";
@@ -75,11 +85,21 @@ public class OperacionesDeComversiones
 		{
 			es = "Son: ";
 		}
+		// Crear una instancia de DecimalFormat para formatear números con dos decimales
 		DecimalFormat decimalFormat = new DecimalFormat("#.00");
+		/*
+		 * Muestra una ventana emergente (cuadro de diálogo), utiliza JOptionPane
+		 *con el resultado formateado usando el DecimalFormat
+		 * y añadiendo el valor de String es.
+		 *El resultado se mostrará con dos decimales debido a la configuración del 
+		 *DecimalFormat.
+		 */
 		JOptionPane.showMessageDialog(null, es +
 		decimalFormat.format(getResultado())+string);
 	}
-
+	//
+	//Y por ultimo se muestra los gets y sets.
+	//
 	public double getValor() {
 		return valor;
 	}
